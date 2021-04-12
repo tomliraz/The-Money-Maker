b2741b9bf54f4e37c9f6596901b41cb7dc06aaa0
justalmill <50275799+justalmill@users.noreply.github.com>
2021-04-12 18:38:18 -0400

    changed Trend Query to YYYY-MM-DD outputs

--- cx_oracle-flask.py
+++ cx_oracle-flask.py
@@ -122,17 +122,17 @@ def get_correlation(Stock1, Stock2, start, stop, interval='D'):
         intervalQuery = f"extract(year from Market_Date)" #example: http://localhost:8081/correlation/AAPL/MSFT/M/2001-01-01/2003-01-01
         namedIntervalQuery = "extract(year from Market_Date) year"
         nameOfInterval = f"year"
-        formatting = "year"
+        formatting = "CONCAT(CONCAT(year, CONCAT('-', '01')), CONCAT('-', '01'))"
     elif (interval == 'M'):
         intervalQuery = f"extract(year from Market_Date), extract(month from Market_Date)" 
         namedIntervalQuery = "extract(year from Market_Date) AS year, extract(month from Market_Date) AS month"
         nameOfInterval = f"year, month"
-        formatting = "CONCAT(CONCAT(month,'-'), year)"
+        formatting = "CONCAT(CONCAT(year, CONCAT('-', LPAD(month, 2, '0'))), CONCAT('-', '01'))"
     elif (interval == 'Q'):
         intervalQuery = f"extract(year from Market_Date), CEIL(extract(month from Market_Date)/3)"
         namedIntervalQuery = "extract(year from Market_Date) AS year, CEIL(extract(month from Market_Date)/3) AS quarter"
         nameOfInterval = f"year, quarter"
-        formatting = "CONCAT(CONCAT(quarter,'-'), year)"
+        formatting = "CONCAT(CONCAT(year, CONCAT('-', LPAD(3*quarter-2, 2,'0'))), CONCAT('-', '01'))"
     #elif (interval == 'W'): # not working
     #    intervalQuery = f"extract(year from Market_Date), TO_CHAR(Market_Date, 'WW')"
     #    namedIntervalQuery = "extract(year from Market_Date) AS year, TO_CHAR(Market_Date, 'WW') AS week"
@@ -147,7 +147,7 @@ def get_correlation(Stock1, Stock2, start, stop, interval='D'):
 
     correlation = f"SELECT {nameOfInterval}, Cov / stddev_Product AS corr FROM ({ summedDemeanedPrices }) NATURAL JOIN ({ stdDevProduct })"
 
-    formattedCorrelation = f"SELECT {formatting}, corr FROM ({correlation})"
+    formattedCorrelation = f"SELECT {formatting}, corr FROM ({correlation}) ORDER BY {formatting}"
 
     cursor.execute(formattedCorrelation)
     r = cursor.fetchall()
@@ -175,21 +175,21 @@ def get_volatility(Stock1, start, stop, interval='D'):
     if (interval == 'Y'):
         intervalQuery = f"extract(year from Market_Date)"
         namedIntervalQuery = "extract(year from Market_Date) year"
-        formatting = "year"
+        formatting = "CONCAT(CONCAT(year, CONCAT('-', '01')), CONCAT('-', '01'))"
     elif (interval == 'M'):
         intervalQuery = f"extract(year from Market_Date), extract(month from Market_Date)"
         namedIntervalQuery = "extract(year from Market_Date) year, extract(month from Market_Date) month"
-        formatting = "CONCAT(CONCAT(month,'-'), year)"
+        formatting = "CONCAT(CONCAT(year, CONCAT('-', LPAD(month, 2, '0'))), CONCAT('-', '01'))"
     elif (interval == 'Q'):
         intervalQuery = f"extract(year from Market_Date), CEIL(extract(month from Market_Date)/3)"
         namedIntervalQuery = "extract(year from Market_Date) year, CEIL(extract(month from Market_Date)/3) quarter"
-        formatting = "CONCAT(CONCAT(quarter,'-'), year)"
+        formatting = "CONCAT(CONCAT(year, CONCAT('-', LPAD(3*quarter-2, 2,'0'))), CONCAT('-', '01'))"
     #elif (interval == 'W'): # not working
     #    intervalQuery = f"extract(year from Market_Date)"
 
     volatility = f"SELECT {namedIntervalQuery}, STDDEV(adj_close)/AVG(adj_close)*100 std FROM LIRAZ.stock_data WHERE stock_id = '{Stock1}' AND Market_Date >= TO_DATE('{start}', 'YYYY-MM-DD') AND Market_Date <= TO_DATE('{stop}', 'YYYY-MM-DD') GROUP BY {intervalQuery}"
 
-    formattedVolatility = f"SELECT {formatting}, std FROM ({volatility})"
+    formattedVolatility = f"SELECT {formatting}, std FROM ({volatility}) ORDER BY {formatting}"
 
     print(formattedVolatility)
 
