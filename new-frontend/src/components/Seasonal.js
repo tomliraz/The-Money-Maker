098ff616fc7e2b9b3a3fc691bba86cc46f305017
Tom Liraz <tomliraz@gmail.com>
2021-04-15 21:12:01 -0400

    Added REACT all components.

---
+++ new-frontend/src/components/Seasonal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ShowGraph from './ShowGraph';
+ 
+function Seasonal() {
+
+    const data = [
+        {
+          label: 'Series 1',
+          data: [[0, -2], [1, 2], [2, 4], [3, 2], [4, 7]]
+        },
+        {
+          label: 'Series 2',
+          data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
+        }
+      ];
+    
+      const axis = [
+        { primary: true, type: 'linear', position: 'bottom' },
+        { type: 'linear', position: 'left' }
+      ];
+
+    return (
+        <div>
+            <ShowGraph data={data} axis={axis}/>
+        </div>
+    );
+}
+export default Seasonal;
