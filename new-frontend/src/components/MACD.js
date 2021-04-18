8f8f29a3b11027d369c6ee3355c2b8476dde6400
Kyle Dampier <kyle.dampier@frischlymade.tech>
2021-04-17 20:42:54 -0400

    fixed date on MACD

--- new-frontend/src/components/MACD.js
+++ new-frontend/src/components/MACD.js
@@ -21,8 +21,10 @@ class MACD extends React.Component {
     })
     .then((response) => response.json())
     .then((response) => {
-      console.log(response);
-      this.data = response;
+      this.data = response.map((arr) => {
+        arr[0] = arr[0].substr(0,10);
+        return arr;
+      });
       this.setState({data: this.data});
     });
   };
@@ -33,6 +35,7 @@ class MACD extends React.Component {
       chartArea: { width: '80%' },
       hAxis: {
         title: 'Date',
+
       },
       vAxis: {
         title: 'Price',
